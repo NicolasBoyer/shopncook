65f997742de7f8a8195341f6fbcdd0ee9b3325de
Nicolas <elendil_le_grand@hotmail.com>
2022-05-04 02:15:18 +0200

    prod test

--- src/back/prod.js
+++ src/back/prod.js
@@ -2,4 +2,4 @@ import { Server } from './server.js'
 import Routes from './routes.js'
 
 // eslint-disable-next-line no-undef
-new Routes(new Server(process.env.PORT))
+new Routes(new Server(25866))
