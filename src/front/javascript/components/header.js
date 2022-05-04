e6df11cb2621711f7c12bd2ab9612b7f3bbb524e
Nicolas <elendil_le_grand@hotmail.com>
2022-05-04 15:05:51 +0200

    image et recipes

--- src/front/javascript/components/header.js
+++ src/front/javascript/components/header.js
@@ -1,14 +1,17 @@
-export default class Header extends HTMLElement {
-	// get src () {
-	//	return this.getAttribute('src')
-	// }
-	//
-	// get alt () {
-	//	return this.getAttribute('alt')
-	// }
+import { html, render } from 'https://cdn.jsdelivr.net/npm/lit-html'
 
-	connectedCallback () {
-		// this.render()
+export default class Header extends HTMLElement {
+	get src () {
+		return this.getAttribute('src')
+	}
+
+	get alt () {
+		return this.getAttribute('alt')
+	}
+
+	constructor () {
+		super()
+		this.render()
 		const image = this.querySelector('img')
 		image.style.transform = `translate3d(0, ${window.scrollY}px, 0)`
 		window.addEventListener('scroll', () => {
@@ -18,11 +21,21 @@ export default class Header extends HTMLElement {
 		})
 	}
 
-	// render () {
-	//	render(html`
-	//		<div class="image">
-	//			<img alt="${this.alt}" src="${this.src}"/>
-	//		</div>
-	//	`, this)
-	// }
+	connectedCallback () {
+		// const image = this.querySelector('img')
+		// image.style.transform = `translate3d(0, ${window.scrollY}px, 0)`
+		// window.addEventListener('scroll', () => {
+		//	image.style.transform = `translate3d(0, ${window.scrollY * 0.5}px, 0)`
+		//	if (window.scrollY >= this.getBoundingClientRect().height) this.firstElementChild.classList.add('titles')
+		//	else this.firstElementChild.classList.remove('titles')
+		// })
+	}
+
+	render () {
+		render(html`
+			<div class="image">
+				<img alt="${this.alt}" src="${this.src}"/>
+			</div>
+		`, this)
+	}
 }
