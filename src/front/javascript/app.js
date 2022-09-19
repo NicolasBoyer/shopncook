b791b40d93cc3d41b0c44ce6b811ec9138792b22
Nicolas <elendil_le_grand@hotmail.com>
2022-09-19 19:35:14 +0200

    todo

--- src/front/javascript/app.js
+++ src/front/javascript/app.js
@@ -19,7 +19,6 @@ class App {
 	// TODO Liste des plats de la semaine
 	// TODO Liste de ce qui est présent dans le congelé
 	// TODO Choix pour activer les fonctionnalités
-	// TODO section animée pour gérer la page de présentation
 	// TODO permettre qu'il existe plusieurs sessions différentes pour que plusieurs personnes puissent l'utiliser sans être sur le même compte
 	// TODO permettre de rendre payant
 	// TODO minifier et combiner les fichiers js clients dans app.js
