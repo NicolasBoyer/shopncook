22f556d8bf56fd9b002f823bf2e23259f3de61ba
nib <nicolas.boyer@kelis.fr>
2024-09-18 15:52:27 +0200

    page 404

--- src/front/javascript/404.ts
+++ src/front/javascript/404.ts
@@ -3,7 +3,7 @@ type TCommands = {
     exit: string
     report: string
     commands: string
-    cls: string
+    clear: string
 }
 
 enum TWindowSize {
@@ -18,8 +18,8 @@ class error404 {
         help: 'La page que vous souhaitez visiter n\'existe pas, ou elle a peut-être été supprimée, ou une mauvaise adresse a été saisie. Pour voir les commandes, entrez le mot <span class="red"> commands</span>',
         exit: '',
         report: '<span class=\'green\'>\n' + 'Ce rapport de page a été envoyé avec succès au support.</span>',
-        commands: 'Liste des commandes : <span class="red"> help</span> , <span class="red"> report</span> ,<span class="red"> exit</span>\n',
-        cls: '',
+        commands: 'Liste des commandes : <span class="red"> help</span>, <span class="red"> report</span>, <span class="red"> exit</span>, <span class="red"> clear</span>\n',
+        clear: '',
     }
     private userInput = document.getElementById('userInput') as HTMLElement
     private terminalOutput = document.getElementById('code') as HTMLElement
@@ -29,9 +29,9 @@ class error404 {
 
     constructor() {
         this.keyboard.focus()
-        if (screen.width < 991) this.keyboard.addEventListener('keyup', (event: KeyboardEvent): void => this.key(event))
-        else document.addEventListener('keypress', (event: KeyboardEvent): void => this.key(event))
-        document.addEventListener('keydown', (event: KeyboardEvent): void => this.backSpace(event))
+        if (screen.width < 991) this.keyboard.addEventListener('keyup', (event: KeyboardEvent): void => this.evalKey(event))
+        else document.addEventListener('keypress', (event: KeyboardEvent): void => this.evalKey(event))
+        document.addEventListener('keydown', (event: KeyboardEvent): void => this.removeEntry(event))
         ;(document.getElementById('min') as HTMLElement).addEventListener('click', (event): void => this.interact(event, TWindowSize.min))
         ;(document.getElementById('max') as HTMLElement).addEventListener('click', (event): void => this.interact(event, TWindowSize.max))
         ;((document.getElementById('min_app') as HTMLElement).querySelector('a') as HTMLElement).addEventListener('click', (event): void => this.interact(event, TWindowSize.re))
@@ -59,13 +59,14 @@ class error404 {
         }
         if (!Object.keys(this.COMMANDS).includes(input)) {
             output = '<p>La commande saisie n\'est pas correcte</p>'
-        } else if (input === 'cls') {
+        } else if (input === 'clear') {
             this.terminalOutput.innerHTML = ''
             return
         } else if (input === 'close' || input === 'exit') {
             location.href = '/'
             return
         } else if (input === 'report') {
+            // window.open('mailto:test@example.com?subject=subject&body=body')
             this.terminalOutput.innerHTML = `${this.terminalOutput.innerHTML}<p>${this.COMMANDS[input]}</p>`
             return
         } else {
@@ -75,7 +76,7 @@ class error404 {
         this.terminal.scrollTop = this.terminalOutput.scrollHeight
     }
 
-    private key(event: KeyboardEvent): void {
+    private evalKey(event: KeyboardEvent): void {
         let currentKey = event.key
         this.keyboard.focus()
         this.keyboard.innerHTML = (event.target as HTMLInputElement).value
@@ -98,7 +99,7 @@ class error404 {
         }
     }
 
-    private backSpace(event: KeyboardEvent): void {
+    private removeEntry(event: KeyboardEvent): void {
         if (event.code === 'Backspace') {
             this.userInput.innerHTML = this.userInput.innerHTML.slice(0, this.userInput.innerHTML.length - 1)
             this.str = this.str.slice(0, this.str.length - 1)
